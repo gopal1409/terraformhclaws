5bc435374108278035159e1d4f97ebfea72d7674
Gopal <Gopal1409@gmail.com>
2021-08-04 11:27:04 -0700

    list

--- ec2.tf
+++ ec2.tf
@@ -1,3 +1,9 @@
+data "aws_availability_zones" "my_zones"{
+    filter {
+        name = "opt-in-status"
+        values = ["opt-in-not-required"]
+    }
+}
 resource "aws_instance" "ec2demo" {
  
     ami = data.aws_ami.amzlinux2.id
@@ -5,7 +11,10 @@ resource "aws_instance" "ec2demo" {
     user_data = file("${path.module}/app1-install.sh")
     vpc_security_group_ids = [aws_security_group.vpc-ssh.id,aws_security_group.vpc-web.id]
     #identifier = expression #argument
+    #this will create ec2 instance in all az
+    for_each = toset(data.aws_availability_zones.my_zones.names)
+    availability_zone = each.key
     tags = {
-        "Name" = "gopal-instance"
+        "Name" = "gopal-demo-${each.value}"
     }
 }
