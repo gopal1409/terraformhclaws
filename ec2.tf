92fbd4a8ae43612ee78baa38583dbcc60db5081a
Gopal <Gopal1409@gmail.com>
2021-08-04 10:18:15 -0700

    security group

--- ec2.tf
+++ ec2.tf
@@ -3,6 +3,7 @@ resource "aws_instance" "ec2demo" {
     ami = data.aws_ami.amzlinux2.id
     instance_type = var.aws_instance
     user_data = file("${path.module}/app1-install.sh")
+    vpc_security_group_ids = [aws_security_group.vpc-ssh.id,aws_security_group.vpc-web.id]
     #identifier = expression #argument
     tags = {
         "Name" = "gopal-instance"
