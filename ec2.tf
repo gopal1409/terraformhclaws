87bae67bc88847d3c701f1eae6d1f7307f548039
Gopal <Gopal1409@gmail.com>
2021-08-05 05:32:28 -0700

    instance type

--- ec2.tf
+++ ec2.tf
@@ -2,7 +2,7 @@
 resource "aws_instance" "ec2demo" {
  
     ami = data.aws_ami.amzlinux2.id
-    instance_type = var.aws_instance
+    instance_type = var.instance_type
     user_data = file("${path.module}/app1-install.sh")
     vpc_security_group_ids = [aws_security_group.vpc-ssh.id,aws_security_group.vpc-web.id]
     #identifier = expression #argument
