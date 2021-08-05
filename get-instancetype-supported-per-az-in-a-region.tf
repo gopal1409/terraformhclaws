5deb81d10d2649c693906545aaac5a1f7545641d
Gopal <Gopal1409@gmail.com>
2021-08-05 06:12:09 -0700

    instance

--- get-instancetype-supported-per-az-in-a-region.tf
+++ get-instancetype-supported-per-az-in-a-region.tf
@@ -38,7 +38,7 @@ output "output_v3_1" {
 output "output_v3_2" {
     value = keys( {
         for az, details in data.aws_ec2_instance_type_offering.my_inst_type:
-        az => details.instance_type if length(details.instance_type)!=0 })[0]
+        az => details.instance_type if length(details.instance_type)!=0 })[1]
   
 }
 
