306f2ff7c2e2bb626953b344f625b1fa8815675c
Gopal <Gopal1409@gmail.com>
2021-08-05 05:00:24 -0700

    for loop

--- ec2.tf
+++ ec2.tf
@@ -1,10 +1,4 @@
-#avaialbility zone datasource
-data "aws_availability_zones" "my_zones"{
-    filter {
-        name = "opt-in-status"
-        values = ["opt-in-not-required"]
-    }
-}
+
 resource "aws_instance" "ec2demo" {
  
     ami = data.aws_ami.amzlinux2.id
@@ -13,7 +7,8 @@ resource "aws_instance" "ec2demo" {
     vpc_security_group_ids = [aws_security_group.vpc-ssh.id,aws_security_group.vpc-web.id]
     #identifier = expression #argument
     #this will create ec2 instance in all az
-    for_each = toset(data.aws_availability_zones.my_zones.names)
+    for_each = toset(keys({for az, details in data.aws_ec2_instance_type_offering.my_inst_type:
+    az => details.instance_type if length(details.instance_type)!=0 }))
     availability_zone = each.key #each value list of items each.key==each.value. 
     tags = {
         "Name" = "gopal-demo-${each.value}"
---
+++ get-instancetype-supported-per-az-in-a-region.tf
@@ -0,0 +1,80 @@
+#get the list of availability zone in a specific region
+#avaialbility zone datasource
+#Datasource 1
+data "aws_availability_zones" "my_zones"{
+    filter {
+        name = "opt-in-status"
+        values = ["opt-in-not-required"]
+    }
+}
+#check if that instance type is supported in that specific region in list of az
+#get the list of az in a particular region where that respective instance type of supported. 
+#datasource-2
+data "aws_ec2_instance_type_offering" "my_inst_type" {
+  filter {
+    name   = "instance-type"
+    values = ["t2.micro"]
+  }
+  filter {
+    name = "location"
+    values = each.key
+  }
+  location_type = "availability-zone"
+}
+
+#output-1
+# all az mapped to support instance type
+output "output_v3_1" {
+    value = {
+        for az, details in data.aws_ec2_instance_type_offering.my_inst_type: az => details.instance_type
+    }
+
+}
+
+#we are going to see which az are not supporting the instance type
+output "output_v3_2" {
+    value = {
+        for az, details in data.aws_ec2_instance_type_offering.my_inst_type:
+        az => details.instance_type if length(details.instance_type)!=0}
+  
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
