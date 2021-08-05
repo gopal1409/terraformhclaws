98d9e5a5f0034f46192893b59b83060a6e22fef7
Gopal <Gopal1409@gmail.com>
2021-08-05 04:01:37 -0700

    for each loop

--- output.tf
+++ output.tf
@@ -5,5 +5,5 @@ output "for_output_list" {
 
 output "for_output_map1" {
     description = "for loop with map"
-    value = toset([for instance in aws_instance.ec2demo: => instance.public_dns])
+    value = toset([for instance in aws_instance.ec2demo: az => instance.public_dns])
 }
