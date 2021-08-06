aec797f085d752391e0969556b468fc78bd9d27a
Gopal <Gopal1409@gmail.com>
2021-08-06 10:22:01 -0700

    backend

--- ec2.tf
+++ ec2.tf
@@ -7,9 +7,9 @@ resource "aws_instance" "ec2demo" {
     vpc_security_group_ids = [aws_security_group.vpc-ssh.id,aws_security_group.vpc-web.id]
     #identifier = expression #argument
     #this will create ec2 instance in all az
-    for_each = toset(keys({for az, details in data.aws_ec2_instance_type_offering.my_inst_type:
-    az => details.instance_type if length(details.instance_type)!=0 }))
-    availability_zone = each.key #each value list of items each.key==each.value. 
+ #   for_each = toset(keys({for az, details in data.aws_ec2_instance_type_offering.my_inst_type:
+  #  az => details.instance_type if length(details.instance_type)!=0 }))
+   # availability_zone = each.key #each value list of items each.key==each.value. 
     tags = {
         "Name" = "gopal-demo-${each.value}"
     }
